9ea3cbb95a85dddb743fb8c016119cecde1589bc
juraj01 <juraj.m.sk@gmail.com>
2024-03-12 17:28:26 +0100

    changed name of form1 and added design for register

--- GunaUI2_Project/EldenRing - Interaktívna mapa - Guna UI/EldenRing - Interaktívna mapa - Guna UI/Register.Designer.cs
+++ GunaUI2_Project/EldenRing - Interaktívna mapa - Guna UI/EldenRing - Interaktívna mapa - Guna UI/Register.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EldenRing___Interaktívna_mapa___Guna_UI
 {
-    partial class Form2
+    partial class Register
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.ExitButton = new Guna.UI2.WinForms.Guna2Button();
+            this.MinimalizeButton = new Guna.UI2.WinForms.Guna2Button();
+            this.MaximalizeButton = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel1.Controls.Add(this.ExitButton);
+            this.guna2Panel1.Controls.Add(this.MinimalizeButton);
+            this.guna2Panel1.Controls.Add(this.MaximalizeButton);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.ForeColor = System.Drawing.Color.Transparent;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(796, 31);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButton.BackgroundImage = global::EldenRing___Interaktívna_mapa___Guna_UI.Properties.Resources.Exit___final_;
+            this.ExitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ExitButton.BorderColor = System.Drawing.Color.Transparent;
+            this.ExitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.ExitButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.ExitButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.ExitButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.ExitButton.FillColor = System.Drawing.Color.Transparent;
+            this.ExitButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.ExitButton.ForeColor = System.Drawing.Color.Black;
+            this.ExitButton.Location = new System.Drawing.Point(756, 0);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(40, 29);
+            this.ExitButton.TabIndex = 4;
+            // 
+            // MinimalizeButton
+            // 
+            this.MinimalizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MinimalizeButton.BackgroundImage = global::EldenRing___Interaktívna_mapa___Guna_UI.Properties.Resources.Minimalize___final;
+            this.MinimalizeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.MinimalizeButton.BorderColor = System.Drawing.Color.Transparent;
+            this.MinimalizeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MinimalizeButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.MinimalizeButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.MinimalizeButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.MinimalizeButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.MinimalizeButton.FillColor = System.Drawing.Color.Transparent;
+            this.MinimalizeButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.MinimalizeButton.ForeColor = System.Drawing.Color.Transparent;
+            this.MinimalizeButton.Location = new System.Drawing.Point(674, 0);
+            this.MinimalizeButton.Name = "MinimalizeButton";
+            this.MinimalizeButton.Size = new System.Drawing.Size(40, 29);
+            this.MinimalizeButton.TabIndex = 6;
+            // 
+            // MaximalizeButton
+            // 
+            this.MaximalizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MaximalizeButton.BackgroundImage = global::EldenRing___Interaktívna_mapa___Guna_UI.Properties.Resources.Maximalize___final;
+            this.MaximalizeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.MaximalizeButton.BorderColor = System.Drawing.Color.Transparent;
+            this.MaximalizeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MaximalizeButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.MaximalizeButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.MaximalizeButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.MaximalizeButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.MaximalizeButton.FillColor = System.Drawing.Color.Transparent;
+            this.MaximalizeButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.MaximalizeButton.ForeColor = System.Drawing.Color.Transparent;
+            this.MaximalizeButton.Location = new System.Drawing.Point(715, 0);
+            this.MaximalizeButton.Name = "MaximalizeButton";
+            this.MaximalizeButton.Size = new System.Drawing.Size(40, 29);
+            this.MaximalizeButton.TabIndex = 5;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::EldenRing___Interaktívna_mapa___Guna_UI.Properties.Resources.LoginRegister_Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(796, 448);
+            this.Controls.Add(this.guna2Panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.guna2Panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Button ExitButton;
+        private Guna.UI2.WinForms.Guna2Button MinimalizeButton;
+        private Guna.UI2.WinForms.Guna2Button MaximalizeButton;
     }
 }
--- GunaUI2_Project/EldenRing - Interaktívna mapa - Guna UI/EldenRing - Interaktívna mapa - Guna UI/register.cs
+++ GunaUI2_Project/EldenRing - Interaktívna mapa - Guna UI/EldenRing - Interaktívna mapa - Guna UI/register.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,57 @@ using System.Windows.Forms;
 
 namespace EldenRing___Interaktívna_mapa___Guna_UI
 {
-    public partial class Form2 : Form
+    public partial class Register : Form
     {
-        public Form2()
+        public Register()
         {
             InitializeComponent();
         }
     }
 }
+
+/*using System;
+using System.Data.SqlClient;
+
+namespace RegistraciaDatabazy
+{
+    class Register
+    {
+        static void Main(string[] args)
+        {
+            // Pripojenie k databaze
+            SqlConnection connection = new SqlConnection("Server=YourServerName;Database=YourDatabaseName;Integrated Security=True;");
+            try
+            {
+                connection.Open();
+
+                // Vytvorenie registračnej tabuľky, ak neexistuje
+                SqlCommand createTableCommand = new SqlCommand("CREATE TABLE IF NOT EXISTS Registrations (ID INT IDENTITY(1,1) PRIMARY KEY, Username NVARCHAR(50), Email NVARCHAR(50), Password NVARCHAR(50))", connection);
+                createTableCommand.ExecuteNonQuery();
+
+                // Vloženie nového záznamu do registračnej tabuľky
+                string username = "TestUser";
+                string email = "testuser@example.com";
+                string password = "TestPassword"; // Môžete použiť bezpečnějšie spôsoby uchovávania hesiel, ako je hašovanie.
+                SqlCommand insertCommand = new SqlCommand("INSERT INTO Registrations (Username, Email, Password) VALUES (@Username, @Email, @Password)", connection);
+                insertCommand.Parameters.AddWithValue("@Username", username);
+                insertCommand.Parameters.AddWithValue("@Email", email);
+                insertCommand.Parameters.AddWithValue("@Password", password);
+                insertCommand.ExecuteNonQuery();
+
+                Console.WriteLine("Registracia bola úspešne vykonaná.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Chyba: " + ex.Message);
+            }
+            finally
+            {
+                // Uzavretie pripojenia k databáze
+                connection.Close();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}*/
