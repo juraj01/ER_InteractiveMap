891962fbc2c4d16f25a9412c609ba28f6f8b67f0
Pinutik <jan.bela007@gmail.com>
2024-03-28 18:48:37 +0100

    Ainsel_River database

---
+++ Databáza/Ainsel_River.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Locations (
+    id INTEGER PRIMARY KEY,
+    location_name TEXT
+);
+
+-- Insert the locations into the table
+INSERT INTO Locations (location_name) VALUES 
+('Ainsel River Well Depths'),
+('Ainsel River Sluice Gate'),
+('Ainsel River Downstream'),
+('Dragonkin Soldier of Nokstella'),
+('Astel, Naturalborn of the Void'),
+('Ainsel River Main'),
+('Nokstella, Eternal City'),
+('Nokstella Waterfall Basin'),
+('Lake of Rot Shoreside'),
+('Grand Cloister');
