1f84312bac709e605a0c332b3a5489a30d59b179
Pinutik <jan.bela007@gmail.com>
2024-03-28 18:41:03 +0100

    database Leyndell

---
+++ Databáza/Leyndell.sql
@@ -0,0 +1,21 @@
+CREATE TABLE  Locations (
+    id INTEGER PRIMARY KEY,
+    location_name TEXT
+);
+
+-- Insert the locations into the table
+INSERT INTO Locations (location_name) VALUES 
+('Erdtree Sanctuary'),
+('East Capital Rampart'),
+('Lower Capital Church'),
+('Avenue Balcony'),
+('West Capital Rampart'),
+('Queen''s Bedchamber'),
+('Fortified Manor, First Floor'),
+('Divine Bridge'),
+('Elden Throne'),
+('Underground Roadside'),
+('Forsaken Depths'),
+('Leyndell Catacombs'),
+('Frenzied Flame Proscription'),
+('Cathedral of the Forsaken');
