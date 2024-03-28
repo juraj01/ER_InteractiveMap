3c8ea18317c938483207af1a74a0299e9c6f4543
Pinutik <jan.bela007@gmail.com>
2024-03-28 18:49:32 +0100

    Siofra_River database

---
+++ Databáza/Siofra_River.sql
@@ -0,0 +1,21 @@
+CREATE TABLE  Locations (
+    id INTEGER PRIMARY KEY,
+    location_name TEXT
+);
+
+-- Insert the locations into the table
+INSERT INTO Locations (location_name) VALUES 
+('Nokron, Eternal City'),
+('Mimic Tear'),
+('Ancestral Woods'),
+('Night''s Sacred Ground'),
+('Aqueduct-Facing Cliffs'),
+('Great Waterfall Basin'),
+('Siofra River Well Depths'),
+('Siofra River Bank'),
+('Worshippers'' Woods'),
+('Below the Well'),
+('Palace Approach Ledge-Road'),
+('Dynasty Mausoleum Entrance'),
+('Dynasty Mausoleum Midpoint'),
+('Cocoon of the Empyrean');
