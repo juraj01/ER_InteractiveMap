908da525a0851f8d0fd69c29ff51840d9f702116
Pinutik <jan.bela007@gmail.com>
2024-03-28 18:28:10 +0100

    datzabase manor

---
+++ Databáza/Volcano_Manor.sql
@@ -0,0 +1,15 @@
+CREATE TABLE  Locations (
+    id INTEGER PRIMARY KEY,
+    location_name TEXT
+);
+
+-- Insert the locations into the table
+INSERT INTO Locations (location_name) VALUES 
+('Volcano Manor'),
+('Prison Town Church'),
+('Temple of Eiglay'),
+('Guest Hall'),
+('Audience Pathway'),
+('Abductor Virgin'),
+('Subterranean Inquisition Chamber'),
+('Rykard, Lord of Blasphemy');
