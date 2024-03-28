3bb0f5fb8350bf3a9d63cc48527b4f0fe3a6c763
Pinutik <jan.bela007@gmail.com>
2024-03-28 18:43:08 +0100

    database Mountains

---
+++ Databáza/Mountains.sql
@@ -0,0 +1,36 @@
+CREATE TABLE  Locations (
+    id INTEGER PRIMARY KEY,
+    location_name TEXT
+);
+
+-- Insert the locations into the table
+INSERT INTO Locations (location_name) VALUES 
+('Forbidden Lands'),
+('Grand Lift of Rold'),
+('Hidden Path to the Haligtree'),
+('Divine Tower of East Altus: Gate'),
+('Divine Tower of East Altus'),
+('Zamor Ruins'),
+('Ancient Snow Valley Ruins'),
+('Freezing Lake'),
+('First Church of Marika'),
+('Whiteridge Road'),
+('Snow Valley Ruins Overlook'),
+('Castle Sol Main Gate'),
+('Church of the Eclipse'),
+('Castle Sol Rooftop'),
+('Spiritcaller Cave'),
+('Giants'' Gravepost'),
+('Church of Repose'),
+('Foot of the Forge'),
+('Fire Giant'),
+('Forge of the Giants'),
+('Giants'' Mountaintop Catacombs'),
+('Giant-Conquering Hero''s Grave'),
+('Consecrated Snowfield'),
+('Inner Consecrated Snowfield'),
+('Ordina, Liturgical Town'),
+('Apostate Derelict'),
+('Consecrated Snowfield Catacombs'),
+('Cave of the Forlorn'),
+('Yelough Anix Tunnel');
