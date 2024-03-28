1cdd2831689d95e299f8aabccf1f49455c2ff33a
Pinutik <jan.bela007@gmail.com>
2024-03-28 18:20:57 +0100

    database stormveil.castle

---
+++ Databáza/Stormveil_Castle.sql
@@ -0,0 +1,14 @@
+CREATE TABLE  Stormveil_Castle_Locations (
+    id INTEGER PRIMARY KEY,
+    location_name TEXT
+);
+
+-- Insert the locations into the table
+INSERT INTO Stormveil_Castle_Locations (location_name) VALUES 
+('Stormveil Main Gate'),
+('Gateside Chamber'),
+('Stormveil Cliffside'),
+('Rampart Tower'),
+('Liftside Chamber'),
+('Secluded Cell'),
+('Godrick the Grafted');
