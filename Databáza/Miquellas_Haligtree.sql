30c6e7950b8fef5c00dd1eb5c7b8757b544ba18e
Pinutik <jan.bela007@gmail.com>
2024-03-28 18:45:23 +0100

    Miquellas_Haligtree database

---
+++ Databáza/Miquellas_Haligtree.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Locations (
+    id INTEGER PRIMARY KEY,
+    location_name TEXT
+);
+
+-- Insert the locations into the table
+INSERT INTO Locations (location_name) VALUES 
+('Haligtree Canopy'),
+('Haligtree Town'),
+('Haligtree Town Plaza'),
+('Haligtree Promenade'),
+('Prayer Room'),
+('Elphael Inner Wall'),
+('Drainage Channel'),
+('Haligtree Roots'),
+('Malenia, Goddess of Rot');
