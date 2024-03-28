82bbe5c4faaa06bf0373c9a47f1e9e950158245d
Pinutik <jan.bela007@gmail.com>
2024-03-28 18:50:42 +0100

    Deeproot_Depths database

---
+++ Databáza/Deeproot_Depths.sql
@@ -0,0 +1,13 @@
+CREATE TABLE  Locations (
+    id INTEGER PRIMARY KEY,
+    location_name TEXT
+);
+
+-- Insert the locations into the table
+INSERT INTO Locations (location_name) VALUES 
+('Root-Facing Cliffs'),
+('Great Waterfall Crest'),
+('Deeproot Depths'),
+('The Nameless Eternal City'),
+('Across the Roots'),
+('Prince of Death''s Throne');
