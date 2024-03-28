a6a5a238dc3974fcc0c5d1976c6297c1216dfade
Pinutik <jan.bela007@gmail.com>
2024-03-28 18:08:02 +0100

    database azula

---
+++ Databáza/Crumbling_Farum_Azula.sql
@@ -0,0 +1,21 @@
+
+
+-- Create a table named 'Crumbling_Farum_Azula' to store the locations
+CREATE TABLE  Crumbling_Farum_Azula (
+    id INTEGER PRIMARY KEY,
+    location_name TEXT
+);
+
+-- Insert the locations into the table
+INSERT INTO Crumbling_Farum_Azula (location_name) VALUES 
+('Crumbling Beast Grave'),
+('Crumbling Beast Grave Depths'),
+('Tempest-Facing Balcony'),
+('Dragon Temple'),
+('Dragon Temple Transept'),
+('Dragon Temple Altar'),
+('Dragon Temple Lift'),
+('Dragon Temple Rooftop'),
+('Beside the Great Bridge'),
+('Dragonlord Placidusax'),
+('Maliketh, the Black Blade');
